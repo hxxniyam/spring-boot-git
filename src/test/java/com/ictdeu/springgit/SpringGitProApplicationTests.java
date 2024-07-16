7e4fc9f9dcfc122efb0f4b0637a8f44e2c2cdfe9
ChoiGaHeun <aprilcgh@naver.com>
2024-07-16 11:53:20 +0900

    Git 학습용 스프링 프로젝트 생성

---
+++ src/test/java/com/ictdeu/springgit/SpringGitProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ictdeu.springgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
