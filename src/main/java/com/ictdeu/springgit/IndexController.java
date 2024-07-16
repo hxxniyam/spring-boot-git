85d4cd30a9e0de2bc8f1ab7b3131660a2ea8cace
ChoiGaHeun <aprilcgh@naver.com>
2024-07-16 12:00:19 +0900

    "/" 라우팅용  IndexController추가

---
+++ src/main/java/com/ictdeu/springgit/IndexController.java
@@ -0,0 +1,11 @@
+package com.ictdeu.springgit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
